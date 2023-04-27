72ec1408571829582d81c773421f4c00dd7c04ff
Hon Boey <hon@dialogicstudios.com.au>
2023-04-27 23:22:12 +1000

    Add print to test Spotify search results

--- six_degrees.py
+++ six_degrees.py
@@ -22,32 +22,42 @@ artist_list = json.load(open("rapper-list.json"))
 first_artist_name = random.choice(artist_list)
 second_artist_name = random.choice(artist_list)
 
+
 # Str -> Object
 # Given the name of a rapper, return their Spotify profile
 def get_artist_spotify_id(str):
-    print(str)
     results = spotify.search(str, limit=1, type="artist")
     try:
         return results["artists"]["items"][0]["id"]
     except IndexError:
         return "No results found"
 
+
 first_artist_spotify_id = get_artist_spotify_id(first_artist_name)
-second_artist_spotify_id =  get_artist_spotify_id(second_artist_name)
+second_artist_spotify_id = get_artist_spotify_id(second_artist_name)
 
 
 # Str, Str -> Str
 def six_degrees_game():
-    print(first_artist_name)
-    print(second_artist_name)
+    print(f"Start at: {first_artist_name}")
+    print(f"Finish at: {second_artist_name}")
     for i in range(6):
-        song_input = print(input(f"{i}. Choose a song: "))
-        artists, track_name = (reveal_song_and_artists(song_input, first_artist_name))
+        song_input = input(f"Song {i + 1}. Choose a song: ")
+        print("song_input", song_input)
+        artists, track_name = reveal_song_and_artists(song_input, first_artist_name)
+
 
 # Str -> Array, Str
 def reveal_song_and_artists(song_name, artist_name):
-    song_details = spotify.search(q=f"remaster%20track:{song_name}%20artist:{artist_name}", limit=1, type="track")
+    print("song_name", song_name)
+    print("search", f"remaster%20track:{song_name} {artist_name}")
+    song_details = spotify.search(
+        q=f"remaster%20track:{song_name} {artist_name}", limit=1, type="track"
+    )
     artists = song_details["tracks"]["items"][0]["artists"]
     track_name = song_details["tracks"]["items"][0]["name"]
+    print(track_name, artists)
     return artists, track_name
-    
+
+
+six_degrees_game()
